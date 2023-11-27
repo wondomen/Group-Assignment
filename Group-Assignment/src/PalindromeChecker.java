dccfc4ee7898dd09a399f18154ddab43e66d5022
Trang Vu <Trang.Vu3@metropolia.fi>
2023-11-27 16:07:22 +0200

    add comments to the codes

--- Group-Assignment/src/PalindromeChecker.java
+++ Group-Assignment/src/PalindromeChecker.java
@@ -3,27 +3,36 @@ import java.util.Scanner;
 public class PalindromeChecker {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String transformed = transformInput(input);
 
-        if (checkPalindrome(transformed)) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string to check for palindrome:");
+        String input = scanner.nextLine();
+
+        String transformedInput = transformInput(input);
+
+        boolean isPalindrome = checkPalindrome(transformedInput);
+        if (isPalindrome) {
             System.out.println("The transformed input is a palindrome.");
         } else {
             System.out.println("The transformed input is not a palindrome.");
         }
-
-        scanner.close();
     }
 
+    // Method to transform the input string by removing commas and exclamation marks, and converting to lowercase
     public static String transformInput(String input) {
-        return input.toLowerCase().replaceAll("[,!]", "");
+        input = input.toLowerCase();
+        input = input.replace(",", "");
+        input = input.replace("!", "");
+        return input;
     }
 
+    // Method to check if a string is a palindrome
     public static boolean checkPalindrome(String input) {
-        return input.equals(reverseString(input)) ? true : false;
+        String reversed = reverseString(input);
+        return input.equals(reversed);
     }
 
+    // Method to reverse a string
     public static String reverseString(String input) {
         StringBuilder reversed = new StringBuilder();
         for (int i = input.length() - 1; i >= 0; i--) {
